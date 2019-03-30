539b373aa5d58ded7032a723bded57af2d18b507
Peanut <michjmend@gmail.com>
2019-03-30 11:33:33 -0400

    html changes

---
+++ data.js
@@ -0,0 +1,10 @@
+$("#add-employee").click(function{
+  event.preventDefault()
+
+  var nameNew = $("#new-employee").val().trim()
+  var roleNew = $("#new-role").val().trim()
+  var startDateNew = $("#start-date").val().trim()
+  var monthsWorked = $("#months-worked").val().trim()
+  var monthlyRate = $("#monthly-rate").val().trim()
+  var totalBilled = $("#total-billed").val().trim() //do not need
+})
